7b71d9f248d6a29973dc79442139c8368054c004
Abdul-Bakri <66844942+Bakri-10@users.noreply.github.com>
2023-05-28 11:51:12 -0600

    Update eks.tf

--- eks.tf
+++ eks.tf
@@ -1,3 +1,12 @@
+provider "aws" {
+  region = "us-west-2"  # Replace with your desired region
+}
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+  # Add necessary configuration for the VPC module
+}
+
 module "eks_cluster" {
   source  = "terraform-aws-modules/eks/aws"
   version = "16.0.0"
@@ -5,9 +14,8 @@ module "eks_cluster" {
   cluster_name     = "my-eks-cluster"
   cluster_version  = "1.26"
 
-  vpc_id = module.vpc.vpc_id
-
-  subnets = module.vpc.private_subnets
+  vpc_id   = module.vpc.vpc_id
+  subnets  = module.vpc.private_subnets
 
   worker_groups = [
     {
@@ -16,25 +24,4 @@ module "eks_cluster" {
       asg_min_size  = 1
     }
   ]
-
-  kubeconfig_aws_authenticator_additional_args = ["--disable-prompt"]
-  config_output_path                          = "./kubeconfig.yaml"
-
-  enable_irsa = true
-
-  tags = {
-    Name = "my-eks-cluster"
-  }
-}
-
-output "cluster_endpoint" {
-  value = module.eks_cluster.cluster_endpoint
-}
-
-output "cluster_certificate_authority_data" {
-  value = module.eks_cluster.cluster_certificate_authority_data
-}
-
-output "aws_authenticator_token" {
-  value = module.eks_cluster.aws_authenticator_token
 }
