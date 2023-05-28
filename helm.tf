3a949632a3fee3ab844a1ec54abe3824cd8989d9
Owolabi precious <owo.pre.eno@gmail.com>
2023-05-28 15:12:55 +0100

    adjusted versions

--- helm.tf
+++ helm.tf
@@ -2,7 +2,7 @@ resource "helm_release" "external_dns" {
   name       = "external-dns"
   repository = "https://charts.bitnami.com/bitnami"
   chart      = "external-dns"
-  version    = "3.7.0"
+  version    = "6.20.3"
 
   set {
     name  = "provider"
@@ -19,7 +19,7 @@ resource "helm_release" "nginx_ingress" {
   name       = "nginx-ingress"
   repository = "https://charts.bitnami.com/bitnami"
   chart      = "nginx-ingress"
-  version    = "10.3.0"
+  version    = "3.1.1"
 
   set {
     name  = "controller.service.type"
