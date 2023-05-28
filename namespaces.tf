e73f457bb7852ba9d402a547206a72b00cf002e6
Owolabi precious <owo.pre.eno@gmail.com>
2023-05-28 16:13:18 +0100

    added config path

--- namespaces.tf
+++ namespaces.tf
@@ -3,6 +3,7 @@ provider "kubernetes" {
   cluster_ca_certificate = base64decode(module.eks_cluster.cluster_certificate_authority_data)
   # token                  = module.eks_cluster.aws_authenticator_token
   # load_config_file       = false
+    config_path = "./kubeconfig.yaml"
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
     command     = "aws"
